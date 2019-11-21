d4c9a9ce372f3590ad33b10b88a9cddb9d0fb064
MIHAJLO JOVANOVIC <mihajloj@gmail.com>
2019-11-21 11:27:04 -0500

    basic demo of auto-complete search box with elasticsearch working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import Autosuggest from 'react-autosuggest';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class AutoComplete extends React.Component {
+    state = {
+	value: '',
+	suggestions: []
+    }
+
+    renderSuggestion = suggestion => {
+	return (
+	    <div className="result">
+		<div className="line">{suggestion.first_name}</div>
+	</div>)
+	
+    }
+
+    onChange = (event, { newValue }) => {
+	this.setState ({ value: newValue })
+    }
+
+    onSuggestionsFetchRequested = ({value}) => {
+	axios.post('http://localhost:9200/people/_search', {
+	    query: {
+		match: {
+		    'first_name': value
+		}
+	    },
+	    sort: ['_score']
+	})
+	    .then (res => {
+		const results = res.data.hits.hits.map (h => h._source)
+		this.setState ({ suggestions: results })
+	    })}
+
+    onSuggestionsClearRequested = () => {
+	this.setState ({suggestions: []})
+    }
+
+    render() {
+	const {value, suggestions} = this.state
+
+	const inputProps = {
+	    placeholder: 'person name',
+	    value,
+	    onChange: this.onChange
+	}
+	return (
+	<div className="App">
+	<h1>Search by persons name</h1>
+		<Autosuggest
+	    suggestions={suggestions}
+	    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+	    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+	    getSuggestionValue={suggestion => suggestion.first_name + ' ' + suggestion.last_name + ' ' + suggestion.skill + ' ' + suggestion.phone}
+	    renderSuggestion={this.renderSuggestion}
+	    inputProps={inputProps}
+		/>
     </div>
   );
 }
 
-export default App;
+}
+
+export default AutoComplete;
